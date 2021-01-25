d7a90aacd0b48b67196e4155b3c4b1690fa46aed
DiegoGamesFight <35296262+diogoditorr@users.noreply.github.com>
2021-01-25 14:27:34 -0300

    Next Level Week #1

---
+++ scripts/intro.js
@@ -0,0 +1,72 @@
+
+// Comments
+// document.write("Hello World")
+
+// variaveis, tipos de dados
+// var myvar = 'He'// undefined
+// const myconst = 'He'
+// let letvar = 'Oi'
+
+
+// document.write(myconst + myvar)
+
+// string
+// const s1 = 'String'
+// const s2 = "String"
+// const s3 = `String`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidade
+// const pessoa = {
+//   altura: '1,80m',
+//   idade: 24,
+//   solteiro: true,
+//   correr() {
+//     document.write('Executar uma grande logica de correr')
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ['blue', 'green', 1, {name: 'My Name'}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Function - Funcionalidades - Atalhos - Reuso de código
+
+// // registrar função
+// function sayMyName(name) {
+//   document.write(name)
+// }
+
+// // Executar
+// sayMyName('Douglas')
+// sayMyName('Valeska')
+// sayMyName('Kyan')
+
+// condicionais
+
+// const notaFinal = 4
+
+// if (notaFinal < 5) {
+//   document.write('Reprovado')
+// } else {
+//   document.write('Aprovado')
+// }
+
+// loop repetições
+
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
+
